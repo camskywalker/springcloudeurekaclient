41e0bf256d6eeda9211f59b3bcdde2d586b76961
lizijian <camskywalker@outlook.com>
2020-01-05 20:31:54 +0800

    Initial commit

---
+++ src/main/java/com/lizijian/springcloudeurekaclient/SpringcloudeurekaclientApplication.java
@@ -0,0 +1,13 @@
+package com.lizijian.springcloudeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudeurekaclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudeurekaclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lizijian/springcloudeurekaclient/SpringcloudeurekaclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizijian.springcloudeurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudeurekaclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
