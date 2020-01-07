6d04c39fe1d3670b3ea3f0c1d083c2ca655f3790
lizijian <camskywalker@outlook.com>
2020-01-07 08:15:22 +0800

    test

--- src/main/java/com/lizijian/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
+++ src/main/java/com/lizijian/springcloudeurekaclient/SpringCloudEurekaClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringcloudeurekaclientApplication {
+public class SpringCloudEurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudeurekaclientApplication.class, args);
+        SpringApplication.run(SpringCloudEurekaClientApplication.class, args);
     }
 
 }
